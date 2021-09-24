3822fa38b35d66ea0284f082eebb95e41d1cb4ed
IlyaAAAA <ilya-sachuk@mail.ru>
2021-09-24 22:41:15 +0300

    added service impl

---
+++ src/main/java/ru/mail/polis/service/sachuk/ilya/ServiceImpl.java
@@ -0,0 +1,16 @@
+package ru.mail.polis.service.sachuk.ilya;
+
+import ru.mail.polis.service.Service;
+
+public class ServiceImpl implements Service {
+
+  @Override
+  public void start() {
+
+  }
+
+  @Override
+  public void stop() {
+
+  }
+}
