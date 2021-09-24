d697e3636bf8cf9d92f70fc02ae77addff59223f
Andrey Cherevichnik <ioctav@icloud.com>
2021-09-24 23:38:32 +0300

    add DAO Artem Drozdov implementation

---
+++ src/main/java/ru/mail/polis/lsm/artem_drozdov/ByteBufferRecordIterator.java
@@ -0,0 +1,50 @@
+package ru.mail.polis.lsm.artem_drozdov;
+
+import ru.mail.polis.lsm.Record;
+
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+final class ByteBufferRecordIterator implements Iterator<Record> {
+    private final ByteBuffer buffer;
+    private final int toOffset;
+
+    ByteBufferRecordIterator(
+            final ByteBuffer buffer,
+            final int fromOffset,
+            final int toOffset) {
+        buffer.position(fromOffset);
+        this.buffer = buffer;
+        this.toOffset = toOffset;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return buffer.position() < toOffset;
+    }
+
+    @Override
+    public Record next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException("Limit is reached");
+        }
+
+        int keySize = buffer.getInt();
+        ByteBuffer key = read(keySize);
+
+        int valueSize = buffer.getInt();
+        if (valueSize == -1) {
+            return Record.tombstone(key);
+        }
+        ByteBuffer value = read(valueSize);
+
+        return Record.of(key, value);
+    }
+
+    private ByteBuffer read(int size) {
+        ByteBuffer result = buffer.slice().limit(size);
+        buffer.position(buffer.position() + size);
+        return result;
+    }
+}
