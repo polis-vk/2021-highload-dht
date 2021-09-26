449f07a478eaf97014874b6478556ab9a9962533
Danila <danilaeremenko@mail.ru>
2021-09-26 20:38:33 +0300

    HL-DHT: seventh codeclimate refactoring(oh, i mean this one..)

--- src/main/java/ru/mail/polis/lsm/artemdrozdov/SSTableDirProcessor.java
+++ src/main/java/ru/mail/polis/lsm/artemdrozdov/SSTableDirProcessor.java
@@ -13,7 +13,10 @@ import static ru.mail.polis.lsm.artemdrozdov.SSTable.COMPACTION_FILE_NAME;
 import static ru.mail.polis.lsm.artemdrozdov.SSTable.SSTABLE_FILE_PREFIX;
 import static ru.mail.polis.lsm.artemdrozdov.SSTable.getIndexFile;
 
-public abstract class SSTableDirProcessor {
+public final class SSTableDirProcessor {
+    private SSTableDirProcessor() {
+    }
+
     public static List<SSTable> loadFromDir(Path dir) throws IOException {
         Path compaction = dir.resolve(COMPACTION_FILE_NAME);
         if (Files.exists(compaction)) {
