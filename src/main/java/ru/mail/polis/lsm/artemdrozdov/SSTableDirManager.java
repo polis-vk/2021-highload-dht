98d2cddb4f310b82360b2b216ab1ccf12e664979
Danila <danilaeremenko@mail.ru>
2021-09-26 21:05:23 +0300

    HL-DHT: maybe this one?

--- src/main/java/ru/mail/polis/lsm/artemdrozdov/SSTableDirManager.java
+++ src/main/java/ru/mail/polis/lsm/artemdrozdov/SSTableDirManager.java
@@ -13,8 +13,8 @@ import static ru.mail.polis.lsm.artemdrozdov.SSTable.COMPACTION_FILE_NAME;
 import static ru.mail.polis.lsm.artemdrozdov.SSTable.SSTABLE_FILE_PREFIX;
 import static ru.mail.polis.lsm.artemdrozdov.SSTable.getIndexFile;
 
-public final class TableDirProcessor {
-    private TableDirProcessor() {
+public final class SSTableDirManager {
+    private SSTableDirManager() {
     }
 
     public static List<SSTable> loadFromDir(Path dir) throws IOException {
