d59787aea288f9f5607f4dbab908b37d022c4ade
ogamoga <cupkek228@gmail.com>
2021-09-26 20:03:26 +0300

    scripts

---
+++ src/main/java/ru/mail/polis/service/ogamoga/ServiceImpl.java
@@ -0,0 +1,9 @@
+package ru.mail.polis.service.ogamoga;
+
+import ru.mail.polis.lsm.DAO;
+import ru.mail.polis.service.Service;
+
+public class ServiceImpl implements Service {
+    public ServiceImpl(int port, DAO dao) {
+    }
+}
