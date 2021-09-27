b2a3a454d478e5692adc06848d098d183d2dd9ba
Danila <danilaeremenko@mail.ru>
2021-09-27 18:46:00 +0300

    HL-DHT: MyConfig refactored into MyConfigFactory

---
+++ src/main/java/ru/mail/polis/service/danilaeremenko/MyConfigFactory.java
@@ -0,0 +1,20 @@
+package ru.mail.polis.service.danilaeremenko;
+
+import one.nio.http.HttpServerConfig;
+import one.nio.server.AcceptorConfig;
+
+final class MyConfigFactory {
+    private MyConfigFactory() {
+    }
+
+    public static HttpServerConfig fromPort(int port) {
+        AcceptorConfig acceptorConfig = new AcceptorConfig();
+        acceptorConfig.port = port;
+        acceptorConfig.reusePort = true;
+
+        HttpServerConfig resConfig = new HttpServerConfig();
+        resConfig.acceptors = new AcceptorConfig[]{acceptorConfig};
+
+        return resConfig;
+    }
+}
