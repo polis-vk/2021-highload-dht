fc0c951668be5df43bbb20a221c18f0acb88c8c7
evil-enot <91pknwdv24@$>
2021-09-27 11:25:58 +0300

    Codeclimate fix 2

--- src/main/java/ru/mail/polis/service/ilia/ServiceImpl.java
+++ src/main/java/ru/mail/polis/service/ilia/ServiceImpl.java
@@ -1,6 +1,12 @@
 package ru.mail.polis.service.ilia;
 
-import one.nio.http.*;
+import one.nio.http.HttpServer;
+import one.nio.http.HttpServerConfig;
+import one.nio.http.HttpSession;
+import one.nio.http.Param;
+import one.nio.http.Path;
+import one.nio.http.Request;
+import one.nio.http.Response;
 import one.nio.server.AcceptorConfig;
 import ru.mail.polis.lsm.DAO;
 import ru.mail.polis.lsm.Record;
@@ -38,7 +44,7 @@ public class ServiceImpl extends HttpServer implements Service {
     }
 
     /**
-     * Processing and executing the request
+     * Processing and executing the request.
      *
      * @param request - http request
      * @param id - entity id
