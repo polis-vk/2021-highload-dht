41b0258a27abfa46f1182820e7339ca87340bc0a
Danila <danilaeremenko@mail.ru>
2021-09-27 18:46:00 +0300

    HL-DHT: MyConfig refactored into MyConfigFactory

--- src/main/java/ru/mail/polis/service/danilaeremenko/BasicService.java
+++ src/main/java/ru/mail/polis/service/danilaeremenko/BasicService.java
@@ -16,7 +16,7 @@ public class BasicService extends HttpServer implements Service {
     private final DaoWrapper daoWrapper;
 
     public BasicService(int port, DAO dao) throws IOException {
-        super(new MyConfig(port));
+        super(MyConfigFactory.fromPort(port));
         this.daoWrapper = new DaoWrapper(dao);
     }
 
