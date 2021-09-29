6f09239b578e11d98e91d4dfc415809eb2d8b7cd
danila.fedorov <danila.fedorov@corp.mail.ru>
2021-09-29 23:20:42 +0300

    Stage 1 added

---
+++ src/main/java/ru/mail/polis/lsm/artem_drozdov/LsmDAO.java
@@ -0,0 +1,142 @@
+package ru.mail.polis.lsm.artem_drozdov;
+
+import ru.mail.polis.lsm.DAO;
+import ru.mail.polis.lsm.DAOConfig;
+import ru.mail.polis.lsm.Record;
+
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.GuardedBy;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NavigableMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class LsmDAO implements DAO {
+
+    private final ConcurrentLinkedDeque<SSTable> tables = new ConcurrentLinkedDeque<>();
+    @SuppressWarnings({"FieldCanBeLocal", "unused"})
+    private final DAOConfig config;
+    private NavigableMap<ByteBuffer, Record> memoryStorage = newStorage();
+    @GuardedBy("this")
+    private int memoryConsumption;
+
+    public LsmDAO(DAOConfig config) throws IOException {
+        this.config = config;
+        List<SSTable> ssTables = SSTable.loadFromDir(config.dir);
+        tables.addAll(ssTables);
+    }
+
+    private static Iterator<Record> merge(List<Iterator<Record>> iterators) {
+        final int size = iterators.size();
+        if (size == 0) {
+            return Collections.emptyIterator();
+        } else if (size == 1) {
+            return iterators.get(0);
+        } else if (size == 2) {
+            return new RecordMergingIterator(
+                    new PeekingIterator<>(iterators.get(0)),
+                    new PeekingIterator<>(iterators.get(1)));
+        }
+        Iterator<Record> left = merge(iterators.subList(0, size / 2));
+        Iterator<Record> right = merge(iterators.subList(size / 2, size));
+        return new RecordMergingIterator(new PeekingIterator<>(left), new PeekingIterator<>(right));
+    }
+
+    @Override
+    public Iterator<Record> range(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
+        synchronized (this) {
+            Iterator<Record> sstableRanges = sstableRanges(fromKey, toKey);
+            Iterator<Record> memoryRange = map(fromKey, toKey).values().iterator();
+            Iterator<Record> iterator =
+                    new RecordMergingIterator(
+                            new PeekingIterator<>(sstableRanges),
+                            new PeekingIterator<>(memoryRange));
+            return new TombstoneFilteringIterator(iterator);
+        }
+    }
+
+    @Override
+    public void upsert(Record record) {
+        synchronized (this) {
+            memoryConsumption += sizeOf(record);
+            if (memoryConsumption > config.memoryLimit) {
+                try {
+                    flush();
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+                memoryConsumption = sizeOf(record);
+            }
+        }
+
+        memoryStorage.put(record.getKey(), record);
+    }
+
+    @Override
+    public void closeAndCompact() {
+        synchronized (this) {
+            final SSTable table;
+            try {
+                table = SSTable.compact(config.dir, range(null, null));
+            } catch (IOException e) {
+                throw new UncheckedIOException("Can't compact", e);
+            }
+            tables.clear();
+            tables.add(table);
+            memoryStorage = newStorage();
+        }
+    }
+
+    private NavigableMap<ByteBuffer, Record> newStorage() {
+        return new ConcurrentSkipListMap<>();
+    }
+
+    private int sizeOf(Record record) {
+        return SSTable.sizeOf(record);
+    }
+
+    @Override
+    public void close() throws IOException {
+        synchronized (this) {
+            flush();
+        }
+    }
+
+    @GuardedBy("this")
+    private void flush() throws IOException {
+        Path dir = config.dir;
+        Path file = dir.resolve(SSTable.SSTABLE_FILE_PREFIX + tables.size());
+
+        SSTable ssTable = SSTable.write(memoryStorage.values().iterator(), file);
+        tables.add(ssTable);
+        memoryStorage = new ConcurrentSkipListMap<>();
+    }
+
+    private Iterator<Record> sstableRanges(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
+        List<Iterator<Record>> iterators = new ArrayList<>(tables.size());
+        for (SSTable ssTable : tables) {
+            iterators.add(ssTable.range(fromKey, toKey));
+        }
+        return merge(iterators);
+    }
+
+    private NavigableMap<ByteBuffer, Record> map(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
+        if (fromKey == null && toKey == null) {
+            return memoryStorage;
+        } else if (fromKey == null) {
+            return memoryStorage.headMap(toKey, false);
+        } else if (toKey == null) {
+            return memoryStorage.tailMap(fromKey, true);
+        } else {
+            return memoryStorage.subMap(fromKey, true, toKey, false);
+        }
+    }
+
+}
