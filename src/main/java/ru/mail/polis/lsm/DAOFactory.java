a14ae48b4d6a43af7318c2ed11f61bb50d9d6cb9
Alena Zakharova <alenazakharovaspb@yandex.ru>
2021-09-29 20:12:51 +0300

    сдаюсь и использую реализацию Артёма Дроздова

--- src/main/java/ru/mail/polis/lsm/DAOFactory.java
+++ src/main/java/ru/mail/polis/lsm/DAOFactory.java
@@ -1,6 +1,6 @@
 package ru.mail.polis.lsm;
 
-import ru.mail.polis.lsm.alyonazakharova.LmsDAO;
+import ru.mail.polis.lsm.artem_drozdov.LsmDAO;
 
 import java.io.IOException;
 
@@ -16,7 +16,7 @@ public final class DAOFactory {
     public static DAO create(DAOConfig config) throws IOException {
         assert config.dir.toFile().exists();
 
-        return new LmsDAO(config);
+        return new LsmDAO(config);
     }
 
 }
