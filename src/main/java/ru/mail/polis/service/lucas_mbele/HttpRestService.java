957d6ddbe0d6302c937c64a0b8cdb94452110421
Luc Prucell <31856838+LucasMbele@users.noreply.github.com>
2021-09-29 16:28:32 +0300

    Update HttpRestService.java

--- src/main/java/ru/mail/polis/service/lucas_mbele/HttpRestService.java
+++ src/main/java/ru/mail/polis/service/lucas_mbele/HttpRestService.java
@@ -2,10 +2,10 @@ package ru.mail.polis.service.lucas_mbele;
 
 import one.nio.http.HttpServer;
 import one.nio.http.HttpSession;
-import one.nio.http.Request;
 import one.nio.http.Param;
 import one.nio.http.HttpServerConfig;
 import one.nio.http.Path;
+import one.nio.http.Request;
 import one.nio.http.Response;
 import one.nio.server.AcceptorConfig;
 import ru.mail.polis.lsm.DAO;
@@ -16,22 +16,21 @@ import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 
-
 public class HttpRestService extends HttpServer implements Service {
     private final DAO dao; // Our Data Object Access
-    public HttpRestService(final int port, final DAO dao) throws IOException {
-        super(ServiceConfig(port));
+    public HttpRestService (final int port, final DAO dao) throws IOException {
+        super(serviceConfig(port));
         this.dao = dao;
     }
 
-    public static HttpServerConfig ServiceConfig(int port)
+    public static HttpServerConfig serviceConfig(int port)
     {
         // Minimal configuration of our service
         HttpServerConfig config = new HttpServerConfig();
         config.acceptors = new AcceptorConfig[]{ServiceUtils.acceptors(port)};
         return config;
     }
-    // We check if our service works
+     // We check if our service works
      @Path("/v0/status")
      public Response status(Request request)
      {
@@ -48,13 +47,13 @@ public class HttpRestService extends HttpServer implements Service {
      }
     //METHODS - GET/PUT/DELETE
     @Path("/v0/entity")
-    public Response entity (Request request, @Param(value = "id",required = true) String id)
+    public Response entity(Request request, @Param(value = "id",required = true) String id)
     {
         if (id.isBlank()) {
             return new Response(Response.BAD_REQUEST, Response.EMPTY);
         }
         // We handle all 3 methods
-        switch(request.getMethod())
+        switch (request.getMethod())
         {
             case Request.METHOD_GET:
                 return get(id);
@@ -68,15 +67,16 @@ public class HttpRestService extends HttpServer implements Service {
     }
     private Response get(String id) {
         ByteBuffer key = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
-        Iterator<Record> key_iterator = dao.range(key,DAO.nextKey(key)); // A key range containing ids whose start from the current id
-        if(key_iterator.hasNext())
+        Iterator<Record> keyIterator = dao.range(key,DAO.nextKey(key)); // A key range containing ids whose start from the current id
+        if (keyIterator.hasNext())
          {
-            Record record = key_iterator.next();
+            Record record = keyIterator.next();
             return new Response(Response.OK , ServiceUtils.extractBytesBuffer(record.getValue()));
          }
-        else{
+        else 
+         {
             return new Response(Response.NOT_FOUND,Response.EMPTY);
-        }
+         }
     }
     private Response put(String id, byte[] body) {
         ByteBuffer key = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
