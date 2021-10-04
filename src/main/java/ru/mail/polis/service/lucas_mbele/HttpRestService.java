7d19579e8a0263ecc29df9fb083864ce70aac366
Luc Prucell <lucprucell@gmail.com>
2021-10-04 17:34:05 +0300

    Adding ThreadPool

--- src/main/java/ru/mail/polis/service/lucas_mbele/HttpRestService.java
+++ src/main/java/ru/mail/polis/service/lucas_mbele/HttpRestService.java
@@ -12,16 +12,21 @@ import ru.mail.polis.lsm.DAO;
 import ru.mail.polis.lsm.Record;
 import ru.mail.polis.service.Service;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class HttpRestService extends HttpServer implements Service {
     private final DAO dao; // Our Data Object Access
+    private final ThreadPoolExecutor executor;
     
-    public HttpRestService(final int port, final DAO dao) throws IOException {
+    public HttpRestService(final int port, final DAO dao,final int poolSize) throws IOException {
         super(serviceConfig(port));
         this.dao = dao;
+        this.executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(poolSize);
     }
 
     public static HttpServerConfig serviceConfig(int port) {
@@ -46,49 +51,82 @@ public class HttpRestService extends HttpServer implements Service {
     
     //METHODS - GET/PUT/DELETE
     @Path("/v0/entity")
-    public Response entity(Request request, @Param(value = "id",required = true) String id) {
+    public void entity(Request request,HttpSession session, @Param(value = "id",required = true) String id) {
+        executor.execute(()-> {
         if (id.isBlank()) {
-            return new Response(Response.BAD_REQUEST, Response.EMPTY);
-           }
+            try {
+                session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+
         // We handle all three methods
         switch (request.getMethod()) {
             case Request.METHOD_GET:
-                return get(id);
+                 get(id,session);
             case Request.METHOD_PUT:
-                return put(id,request.getBody());
+                 put(id,request.getBody(),session);
             case Request.METHOD_DELETE:
-                return delete(id);
+                 delete(id,session);
             default:
-                return new Response(Response.METHOD_NOT_ALLOWED,"Method not allowed".getBytes(StandardCharsets.UTF_8));
-           }
+                try {
+                    session.sendResponse( new Response(Response.METHOD_NOT_ALLOWED,"Method not allowed".getBytes(StandardCharsets.UTF_8)));
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+        }
+        }});
     }
     
-    private Response get(String id) {
+    private void get(String id,HttpSession session) {
+        Response response;
         ByteBuffer key = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
         Iterator<Record> keyIterator = dao.range(key,DAO.nextKey(key)); //A key range ids started from current id
         if (keyIterator.hasNext()) {
             Record record = keyIterator.next();
-            return new Response(Response.OK,ServiceUtils.extractBytesBuffer(record.getValue()));
+            response = new Response(Response.OK,ServiceUtils.extractBytesBuffer(record.getValue()));
         } else {
-            return new Response(Response.NOT_FOUND,Response.EMPTY);
+            response = new Response(Response.NOT_FOUND,Response.EMPTY);
            }
+
+        try {
+            session.sendResponse(response);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
     
-    private Response put(String id, byte[] body) {
+    private void put(String id, byte[] body,HttpSession session) {
+
         ByteBuffer key = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
         ByteBuffer value = ByteBuffer.wrap(body);
         dao.upsert(Record.of(key,value));
-        return new Response(Response.CREATED,Response.EMPTY);
+        try {
+            session.sendResponse(new Response(Response.CREATED,Response.EMPTY));
+        } catch (IOException e) {
+           throw new UncheckedIOException(e);
+        }
     }
 
-    private Response delete(String id) {
+    private void delete(String id,HttpSession session) {
         ByteBuffer key = ByteBuffer.wrap(id.getBytes(StandardCharsets.UTF_8));
         dao.upsert(Record.tombstone(key));
-        return new Response(Response.ACCEPTED, Response.EMPTY);
+        try {
+            session.sendResponse( new Response(Response.ACCEPTED, Response.EMPTY));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     @Override
-    public void handleDefault(Request request, HttpSession session) throws IOException {
-        session.sendResponse(new Response(Response.BAD_REQUEST,Response.EMPTY));
+    public void handleDefault(Request request, HttpSession session) {
+        executor.execute(() -> {
+            try {
+                session.sendResponse(new Response(Response.BAD_REQUEST,Response.EMPTY));
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        })
+       ;
     }
 }
