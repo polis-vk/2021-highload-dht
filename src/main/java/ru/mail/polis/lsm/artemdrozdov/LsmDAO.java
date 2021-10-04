2d122e877899f2bac7064b8cdc8313b51b0fd3a3
Alexey <medvedev.lasta@yandex.ru>
2021-10-04 22:31:10 +0300

    Fixed codeclimate issues

--- src/main/java/ru/mail/polis/lsm/artemdrozdov/LsmDAO.java
+++ src/main/java/ru/mail/polis/lsm/artemdrozdov/LsmDAO.java
@@ -23,7 +23,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class LsmDAO implements DAO {
