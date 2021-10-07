8dcbcdf11f937a2f14b83a1d041ad59518672a0f
Anastasia Tushkanova <anastasia.tushkanova@corp.mail.ru>
2021-10-07 03:43:55 +0300

    code style fixes

--- src/main/java/ru/mail/polis/lsm/artem_drozdov/LsmDAO.java
+++ src/main/java/ru/mail/polis/lsm/artem_drozdov/LsmDAO.java
@@ -1,7 +1,5 @@
 package ru.mail.polis.lsm.artem_drozdov;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ru.mail.polis.lsm.DAO;
 import ru.mail.polis.lsm.DAOConfig;
 import ru.mail.polis.lsm.Record;
