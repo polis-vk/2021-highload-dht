778de9c7785b094ded182b2a5669083dd247893b
Eldar <eldar.tim@gmail.com>
2021-10-07 16:05:17 +0300

    Tombstones filter iterator separated now

--- src/main/java/ru/mail/polis/lsm/artem_drozdov/LsmDAO.java
+++ src/main/java/ru/mail/polis/lsm/artem_drozdov/LsmDAO.java
@@ -229,30 +229,6 @@ public class LsmDAO implements DAO {
     }
 
     private static Iterator<Record> filterTombstones(Iterator<Record> iterator) {
-        PeekingIterator delegate = new PeekingIterator(iterator);
-        return new Iterator<>() {
-            @Override
-            public boolean hasNext() {
-                for (;;) {
-                    Record peek = delegate.peek();
-                    if (peek == null) {
-                        return false;
-                    }
-                    if (!peek.isTombstone()) {
-                        return true;
-                    }
-
-                    delegate.next();
-                }
-            }
-
-            @Override
-            public Record next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException("No elements");
-                }
-                return delegate.next();
-            }
-        };
+        return new TombstonesFilterIterator(iterator);
     }
 }
