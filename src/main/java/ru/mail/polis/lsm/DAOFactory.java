6ccff249d53ad5519d6fe45bd629699183e485ab
Gasparyan0xff <kantkerry@mail.ru>
2021-10-11 18:49:04 +0300

    fix

--- src/main/java/ru/mail/polis/lsm/DAOFactory.java
+++ src/main/java/ru/mail/polis/lsm/DAOFactory.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public final class DAOFactory {
 
-    private static final int QUEUE_SIZE = 1;
+    private static final int QUEUE_SIZE = 4;
 
     private DAOFactory() {
         // Only static methods
