fae3d2de9705c5ccce2c394b760387d3d4454f62
IlyaAAAA <ilya-sachuk@mail.ru>
2021-10-12 13:21:22 +0300

    f

--- src/main/java/ru/mail/polis/lsm/DAOConfig.java
+++ src/main/java/ru/mail/polis/lsm/DAOConfig.java
@@ -4,16 +4,23 @@ import java.nio.file.Path;
 
 public class DAOConfig {
     public static final int DEFAULT_MEMORY_LIMIT = 4 * 1024 * 1024;
+    public static final int DEFAULT_MAX_TABLES = 20;
 
     public final Path dir;
     public final int memoryLimit;
+    public final int maxTables;
 
     public DAOConfig(Path dir) {
         this(dir, DEFAULT_MEMORY_LIMIT);
     }
 
     public DAOConfig(Path dir, int memoryLimit) {
+        this(dir, memoryLimit, DEFAULT_MAX_TABLES)
+    }
+
+    public DAOConfig(Path dir, int memoryLimit, int maxTable) {
         this.dir = dir;
         this.memoryLimit = memoryLimit;
+        this.maxTables = maxTable;
     }
 }
