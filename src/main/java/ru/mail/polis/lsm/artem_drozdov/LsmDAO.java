c0a92ba26de67e81acb8f3c0ca3dd9220acbaad5
avightclav <avightclav@gmail.com>
2021-10-14 01:30:22 +0300

    async flush bug fixes

--- src/main/java/ru/mail/polis/lsm/artem_drozdov/LsmDAO.java
+++ src/main/java/ru/mail/polis/lsm/artem_drozdov/LsmDAO.java
@@ -3,222 +3,136 @@ package ru.mail.polis.lsm.artem_drozdov;
 import ru.mail.polis.lsm.DAO;
 import ru.mail.polis.lsm.DAOConfig;
 import ru.mail.polis.lsm.Record;
-import ru.mail.polis.lsm.artem_drozdov.util.RecordIterators;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.NavigableMap;
-import java.util.SortedMap;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+
 
 public class LsmDAO implements DAO {
 
     private final long flushMemoryThreshold;
-    private ExecutorService executorService = Executors.newWorkStealingPool();
-    private final AtomicInteger runningFlushesCount = new AtomicInteger(0);
 
+    private final ExecutorService executor = Executors.newWorkStealingPool();
+    private final NavigableMap<Integer, CompletableFuture<?>> runningFlushes = new ConcurrentSkipListMap<>();
     private final AtomicInteger currentIndex = new AtomicInteger(0);
-    private final ConcurrentLinkedDeque<NavigableMap<ByteBuffer, Record>> flushingStorages =
-            new ConcurrentLinkedDeque<>();
-    private NavigableMap<ByteBuffer, Record> memoryStorage;
-    private final ConcurrentLinkedDeque<SSTable> tables = new ConcurrentLinkedDeque<>();
-    private final ArrayDeque<Future<?>> runningFlushes = new ArrayDeque<>();
+    private volatile Storage storage;
 
     @SuppressWarnings({"FieldCanBeLocal", "unused"})
     private final DAOConfig config;
 
-    private final AtomicInteger memoryConsumption = new AtomicInteger();
-
     public LsmDAO(DAOConfig config) throws IOException {
         this.config = config;
-        this.flushMemoryThreshold = config.memoryLimit * 2L;
+        this.flushMemoryThreshold = (long) (config.memoryLimit * 2.5);
         List<SSTable> ssTables = SSTable.loadFromDir(config.dir);
-        tables.addAll(ssTables);
-        currentIndex.set(tables.size());
-        memoryStorage = newStorage();
+        storage = new Storage(ssTables);
+        currentIndex.set(storage.tablesSize());
     }
 
     @Override
     public Iterator<Record> range(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
-        Iterator<Record> sstableRanges = sstableRanges(fromKey, toKey);
-        Iterator<Record> memoryRange;
-        synchronized (this) {
-            memoryRange = new RecordIterators.MergingIterator(
-                    new RecordIterators.PeekingIterator(
-                            RecordIterators.merge(flushingStorages.stream().map(storage -> map(storage, fromKey, toKey))
-                                    .collect(Collectors.toList()))
-                    ),
-                    new RecordIterators.PeekingIterator(map(memoryStorage, fromKey, toKey)));
-        }
-        Iterator<Record> iterator = new RecordIterators.MergingIterator(
-                new RecordIterators.PeekingIterator(sstableRanges),
-                new RecordIterators.PeekingIterator(memoryRange)
-        );
-        return RecordIterators.filterTombstones(iterator);
+        return storage.range(fromKey, toKey);
     }
 
     @Override
     public void upsert(Record record) {
-        if (memoryConsumption.addAndGet(sizeOf(record)) > config.memoryLimit) {
+        if (storage.getCurrentSize() > config.memoryLimit) {
+            while (Runtime.getRuntime().freeMemory() < this.flushMemoryThreshold) {
+                Map.Entry<Integer, CompletableFuture<?>> flushToWait = runningFlushes.firstEntry();
+                if (flushToWait == null) {
+                    break;
+                }
+                flushToWait.getValue().join();
+            }
+
             synchronized (this) {
-                if (memoryConsumption.get() > config.memoryLimit) {
-                    runningFlushes.removeIf(Future::isDone);
-                    while (!runningFlushes.isEmpty() && runningFlushes.peek().isDone()) {
-                        runningFlushes.poll();
-                    }
-
-                    while (Runtime.getRuntime().freeMemory() < this.flushMemoryThreshold && !runningFlushes.isEmpty()) {
-                        try {
-                            runningFlushes.poll().get();
-                        } catch (ExecutionException e) {
-                            break;
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-
-                    synchronized (memoryConsumption) {
-                        final int flushIndex = currentIndex.getAndAdd(1);
-                        final Future<?> future = executorService.submit(
-                                this.runnableFlush(flushIndex, memoryStorage, memoryConsumption.get(),
-                                        getLastSubmittedFlush())
-                        );
-                        runningFlushes.add(future);
-
-                        flushingStorages.add(memoryStorage);
-                        memoryStorage = newStorage();
-                        memoryConsumption.set(sizeOf(record));
-                    }
+                if (storage.getCurrentSize() > config.memoryLimit) {
+                    final int flushIndex = currentIndex.getAndIncrement();
+                    storage = storage.willStartFlush(flushIndex);
+                    CompletableFuture<?> future = CompletableFuture.runAsync(() -> flush(flushIndex, storage),
+                            executor
+                    );
+                    runningFlushes.put(flushIndex, future);
                 }
             }
         }
-        memoryStorage.put(record.getKey(), record);
+        storage.put(record);
     }
 
     @Override
-    public void closeAndCompact() {
+    public void compact() {
         synchronized (this) {
             final SSTable table;
             try {
                 table = SSTable.compact(config.dir, range(null, null));
+                storage = new Storage(Collections.singletonList(table));
+                currentIndex.set(1);
             } catch (IOException e) {
                 throw new UncheckedIOException("Can't compact", e);
             }
-            tables.clear();
-            tables.add(table);
-            memoryStorage = newStorage();
-            currentIndex.set(1);
         }
     }
 
-    private NavigableMap<ByteBuffer, Record> newStorage() {
-        return new ConcurrentSkipListMap<>();
-    }
-
-    private int sizeOf(Record record) {
+    public static int sizeOf(Record record) {
         return SSTable.sizeOf(record);
     }
 
     @Override
     public void close() throws IOException {
-        synchronized (this) {
-            executorService.shutdown();
-            try {
-                if (!executorService.awaitTermination(30, TimeUnit.MINUTES)) {
-                    executorService.shutdownNow();
-                }
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+        executor.shutdown();
+        try {
+            if (!executor.awaitTermination(30, TimeUnit.MINUTES)) {
+                throw new IllegalStateException("Can't wait for termination");
             }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new IllegalStateException(e);
+        }
 
-            executorService = Executors.newWorkStealingPool();
-            synchronized (memoryConsumption) {
-                final int flushIndex = currentIndex.getAndIncrement();
-                try {
-                    flush(flushIndex, memoryStorage, memoryConsumption.get(), getLastSubmittedFlush());
-                } catch (ExecutionException e) {
-                    if (e.getCause() instanceof IOException) {
-                        throw new UncheckedIOException((IOException) e.getCause());
-                    }
-                }
-                memoryConsumption.set(0);
-                memoryStorage = newStorage();
-            }
+        synchronized (this) {
+            final int flushIndex = currentIndex.getAndIncrement();
+            storage = storage.willStartFlush(flushIndex);
+            flush(flushIndex, storage);
+            storage = null;
         }
     }
 
-    private synchronized @Nullable Future<?> getLastSubmittedFlush() {
-        return runningFlushes.isEmpty() ? null : runningFlushes.peek();
-    }
-
-    private Callable<Void> runnableFlush(int index, NavigableMap<ByteBuffer, Record> memoryStorage,
-                                   int memoryTableConsumption, @Nullable Future<?> prevFlush) {
-        return () -> {
-            this.runningFlushesCount.incrementAndGet();
-            flush(index, memoryStorage, memoryTableConsumption, prevFlush);
-            this.runningFlushesCount.decrementAndGet();
-            return null;
-        };
-    }
-
-    private void flush(int index,
-                       NavigableMap<ByteBuffer, Record> memoryStorage,
-                       int memoryTableConsumption,
-                       @Nullable Future<?> prevFlush) throws ExecutionException {
+    private void flush(int index, Storage storageToFlush) {
         try {
             Path dir = config.dir;
             Path file = dir.resolve(SSTable.SSTABLE_FILE_PREFIX + index);
-            SSTable ssTable = SSTable.write(memoryStorage.values().iterator(), file);
+            SSTable ssTable = SSTable.write(
+                    storageToFlush.getFlushingStorages().get(index).getMemory().values().iterator(),
+                    file
+            );
 
-            if (prevFlush != null) {
-                prevFlush.get();
+            CompletableFuture<?> previousFlush = runningFlushes.get(index - 1);
+            if (previousFlush != null) {
+                previousFlush.join();
             }
-            tables.add(ssTable);
-            flushingStorages.poll();
-        } catch (ExecutionException | IOException | InterruptedException e) {
-            synchronized (memoryConsumption) {
-                this.memoryStorage.putAll(memoryStorage);
-                memoryConsumption.getAndAdd(memoryTableConsumption);
+
+            synchronized (this) {
+                storage = storage.doneFlush(index, ssTable);
             }
-            throw new ExecutionException(e);
+        } catch (IOException e) {
+            synchronized (this) {
+                storage = storage.restoreMemory(index);
+            }
+        } finally {
+            runningFlushes.remove(index);
         }
-    }
-
-    private Iterator<Record> sstableRanges(@Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
-        List<Iterator<Record>> iterators = new ArrayList<>(tables.size());
-        for (SSTable ssTable : tables) {
-            iterators.add(ssTable.range(fromKey, toKey));
-        }
-        return RecordIterators.merge(iterators);
-    }
-
-    private Iterator<Record> map(SortedMap<ByteBuffer, Record> map,
-                                 @Nullable ByteBuffer fromKey, @Nullable ByteBuffer toKey) {
-        if (fromKey == null && toKey == null) {
-            return map.values().iterator();
-        }
-        if (fromKey == null) {
-            return map.headMap(toKey).values().iterator();
-        }
-        if (toKey == null) {
-            return map.tailMap(fromKey).values().iterator();
-        }
-        return map.subMap(fromKey, toKey).values().iterator();
     }
 }
---
+++ src/main/java/ru/mail/polis/lsm/artem_drozdov/util/LargeMappedByteBuffer.java
@@ -0,0 +1,6 @@
+package ru.mail.polis.lsm.artem_drozdov.util;
+
+import one.nio.mem.MappedFile;
+
+//public class LargeMappedByteBuffer extends MappedFile {
+//}
