27dc413750ca9786ea01cbf0bb08224846a5cb51
Shabinsky Dmitry <sdimosikvip@gmail.com>
2021-10-13 03:13:11 +0300

    remove volatile from Storage instance

--- src/main/java/ru/mail/polis/lsm/artem_drozdov/LsmDAO.java
+++ src/main/java/ru/mail/polis/lsm/artem_drozdov/LsmDAO.java
@@ -22,7 +22,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class LsmDAO implements DAO {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LsmDAO.class);
-    private volatile Storage storage;
+    private Storage storage;
 
     @SuppressWarnings({"FieldCanBeLocal", "unused"})
     private final DAOConfig config;
