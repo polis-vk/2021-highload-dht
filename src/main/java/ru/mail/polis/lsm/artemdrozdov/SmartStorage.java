28d6648dd440fbbce190e63dd7a4ed9500c118ce
Danila <danilaeremenko@mail.ru>
2021-10-14 13:53:48 +0300

    HL-DHT: SmartStorage class initialized, some problems fixed

---
+++ src/main/java/ru/mail/polis/lsm/artemdrozdov/SmartStorage.java
@@ -0,0 +1,51 @@
+package ru.mail.polis.lsm.artemdrozdov;
+
+import ru.mail.polis.lsm.Record;
+
+import java.nio.ByteBuffer;
+import java.util.NavigableMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class SmartStorage {
+    final NavigableMap<ByteBuffer, Record> memory;
+    final NavigableMap<ByteBuffer, Record> memorySnapshot;
+    final ConcurrentLinkedDeque<SSTable> tables;
+
+
+    private SmartStorage() {
+        memory = getEmptyMap();
+        memorySnapshot = getEmptyMap();
+        tables = new ConcurrentLinkedDeque<>();
+    }
+
+    private SmartStorage(
+            NavigableMap<ByteBuffer, Record> memory,
+            NavigableMap<ByteBuffer, Record> memorySnapshot,
+            ConcurrentLinkedDeque<SSTable> tables
+    ) {
+        this.memory = memory;
+        this.memorySnapshot = memorySnapshot;
+        this.tables = tables;
+    }
+
+
+    private static NavigableMap<ByteBuffer, Record> getEmptyMap() {
+        return new ConcurrentSkipListMap<>();
+    }
+
+    public static SmartStorage createEmpty() {
+        return new SmartStorage();
+    }
+
+    public static SmartStorage createFromTables(ConcurrentLinkedDeque<SSTable> tables) {
+        return new SmartStorage(getEmptyMap(), getEmptyMap(), tables);
+    }
+
+
+    public static SmartStorage createFromSnapshot(NavigableMap<ByteBuffer, Record> memorySnapshot, ConcurrentLinkedDeque<SSTable> tables) {
+        return new SmartStorage(getEmptyMap(), memorySnapshot, tables);
+    }
+
+
+}
