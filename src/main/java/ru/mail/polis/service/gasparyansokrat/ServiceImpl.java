9a2997b7612ebc1554f18d18361e194692d45a9f
Gasparyan0xff <kantkerry@mail.ru>
2021-10-17 18:01:44 +0300

    fix codeclimate notes

--- src/main/java/ru/mail/polis/service/gasparyansokrat/ServiceImpl.java
+++ src/main/java/ru/mail/polis/service/gasparyansokrat/ServiceImpl.java
@@ -1,14 +1,15 @@
 package ru.mail.polis.service.gasparyansokrat;
 
-import ru.mail.polis.lsm.DAO;
-import ru.mail.polis.service.Service;
-
 import one.nio.http.HttpServer;
 import one.nio.http.HttpSession;
 import one.nio.http.Request;
 import one.nio.http.Response;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import ru.mail.polis.lsm.DAO;
+import ru.mail.polis.service.Service;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
