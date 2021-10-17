467c0ee5b675a88f615c5f592ca6fdd2274af6dc
Alexey <medvedev.lasta@yandex.ru>
2021-10-17 18:04:15 +0300

    Added executor to ServiceImpl

--- src/main/java/ru/mail/polis/service/medalexey/ServiceImpl.java
+++ src/main/java/ru/mail/polis/service/medalexey/ServiceImpl.java
@@ -3,48 +3,96 @@ package ru.mail.polis.service.medalexey;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;
-import one.nio.http.Param;
-import one.nio.http.Path;
 import one.nio.http.Request;
 import one.nio.http.Response;
 import one.nio.server.AcceptorConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.mail.polis.lsm.DAO;
 import ru.mail.polis.lsm.Record;
+import ru.mail.polis.lsm.artemdrozdov.LsmDAO;
 import ru.mail.polis.service.Service;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ServiceImpl extends HttpServer implements Service {
+
+    private static final String STATUS_PATH = "/v0/status";
+    private static final String ENTITY_PATH = "/v0/entity";
+    private static final String ID_PARAMETER_KEY = "id";
+    private static final int QUEUE_CAPACITY = 100;
+    private final ThreadPoolExecutor executor;
+    private final Logger logger;
+
     private final DAO dao;
 
     public ServiceImpl(final int port, final DAO dao, int threads) throws IOException {
-        super(from(port, threads));
+        super(from(port));
         this.dao = dao;
+        logger = LoggerFactory.getLogger(LsmDAO.class);
+        this.executor = new ThreadPoolExecutor(threads, threads, 0L,
+                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(QUEUE_CAPACITY));
     }
 
-    private static HttpServerConfig from(final int port, int threads) {
+    private static HttpServerConfig from(final int port) {
         final HttpServerConfig config = new HttpServerConfig();
         final AcceptorConfig acceptor = new AcceptorConfig();
 
         acceptor.port = port;
         acceptor.reusePort = true;
-        acceptor.threads = threads;
         config.acceptors = new AcceptorConfig[]{acceptor};
 
         return config;
     }
 
-    @Override
-    public void handleDefault(Request request, HttpSession session) throws IOException {
-        session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
-    }
-
-    @Path("/v0/status")
     public Response status() {
         return Response.ok("I'm ok");
+    }
+
+    private void handleRequestTask(Request request, HttpSession session)  {
+        Response response;
+        String requestPath = request.getPath();
+
+        switch (requestPath) {
+            case STATUS_PATH: {
+                response = status();
+                break;
+            }
+            case ENTITY_PATH: {
+                String id = request.getParameter(ID_PARAMETER_KEY);
+                response = entity(request, id);
+                break;
+            }
+            default: {
+                response = new Response(Response.BAD_REQUEST, Response.EMPTY);
+            }
+        }
+        try {
+            session.sendResponse(response);
+        } catch (IOException e) {
+            logger.warn("Error sending response", e);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void handleRequest(Request request, HttpSession session) throws IOException {
+        if (executor.getQueue().remainingCapacity() > 0) {
+            synchronized (this) {
+                if (executor.getQueue().remainingCapacity() > 0) {
+                    executor.submit(() -> handleRequestTask(request, session));
+                    return;
+                }
+            }
+        }
+
+        session.sendResponse(new Response(Response.INTERNAL_ERROR, Response.EMPTY));
     }
 
     /**
@@ -54,9 +102,7 @@ public class ServiceImpl extends HttpServer implements Service {
      * @param id entity id
      * @return http response (2xx if ok, 4xx if error)
      */
-    @Path("/v0/entity")
-    public Response entity(final Request request,
-                           @Param(value = "id", required = true) final String id) {
+    public Response entity(final Request request, final String id) {
         if (id.isBlank()) {
             return new Response(Response.BAD_REQUEST, "Bad id".getBytes(StandardCharsets.UTF_8));
         }
@@ -100,5 +146,11 @@ public class ServiceImpl extends HttpServer implements Service {
         } else {
             return new Response(Response.NOT_FOUND, Response.EMPTY);
         }
+    }
+
+    @Override
+    public synchronized void stop() {
+        executor.shutdown();
+        super.stop();
     }
 }
