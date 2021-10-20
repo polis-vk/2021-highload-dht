43d80c9ab0d62a4e7397fd7ab6258117e5b1aa15
alexey.korneev <alexey.korneev@corp.mail.ru>
2021-10-20 21:59:32 +0300

    stage 3 (without report)

--- src/main/java/ru/mail/polis/service/alex/AlexServer.java
+++ src/main/java/ru/mail/polis/service/alex/AlexServer.java
@@ -7,20 +7,37 @@ import one.nio.http.Param;
 import one.nio.http.Path;
 import one.nio.http.Request;
 import one.nio.http.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.mail.polis.lsm.DAO;
 import ru.mail.polis.lsm.Record;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class AlexServer extends HttpServer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AlexServer.class);
+
     private final DAO dao;
+    private final ExecutorService executorService;
 
     public AlexServer(final HttpServerConfig config, final DAO dao) throws IOException {
         super(config);
         this.dao = dao;
+        int cores = Runtime.getRuntime().availableProcessors();
+        this.executorService = new ThreadPoolExecutor(
+                cores,
+                cores,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(cores + 1)
+        );
     }
 
     @Override
@@ -35,24 +52,49 @@ public class AlexServer extends HttpServer {
     }
 
     @Path("/v0/entity")
-    public Response entity(
+    public void entity(
             final Request request,
+            final HttpSession httpSession,
             @Param(value = "id", required = true) final String entityId) {
 
         if (entityId.isEmpty()) {
-            return new Response(Response.BAD_REQUEST, "Empty ID!".getBytes(StandardCharsets.UTF_8));
+            try {
+                httpSession.sendResponse(
+                        new Response(Response.BAD_REQUEST, "Empty ID!".getBytes(StandardCharsets.UTF_8))
+                );
+            } catch (IOException e) {
+                LOGGER.error("Can't response to user.", e);
+            }
         }
 
-        switch (request.getMethod()) {
-            case Request.METHOD_GET:
-                return getEntity(entityId);
-            case Request.METHOD_PUT:
-                return putEntity(entityId, request.getBody());
-            case Request.METHOD_DELETE:
-                return deleteEntity(entityId);
-            default:
-                return new Response(Response.METHOD_NOT_ALLOWED, "Wrong method!".getBytes(StandardCharsets.UTF_8));
-        }
+        executorService.execute(() -> {
+            try {
+                switch (request.getMethod()) {
+                    case Request.METHOD_GET:
+                        httpSession.sendResponse(
+                                getEntity(entityId)
+                        );
+                        break;
+                    case Request.METHOD_PUT:
+                        httpSession.sendResponse(
+                                putEntity(entityId, request.getBody())
+                        );
+                        break;
+                    case Request.METHOD_DELETE:
+                        httpSession.sendResponse(
+                                deleteEntity(entityId)
+                        );
+                        break;
+                    default:
+                        httpSession.sendResponse(
+                                new Response(Response.METHOD_NOT_ALLOWED, "Wrong method!".getBytes(StandardCharsets.UTF_8))
+                        );
+                        break;
+                }
+            } catch (IOException e) {
+                LOGGER.error("Can't response to user.", e);
+            }
+        });
     }
 
     private Response getEntity(final String id) {
