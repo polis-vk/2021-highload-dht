10dffd120ac805066f072a739937e51bb11c3032
Danila <danilaeremenko@mail.ru>
2021-10-20 22:17:12 +0300

    HL-DHT: all parsing logic removed from selectors thread, only io logic left

--- src/main/java/ru/mail/polis/service/danilaeremenko/BasicService.java
+++ src/main/java/ru/mail/polis/service/danilaeremenko/BasicService.java
@@ -2,10 +2,10 @@ package ru.mail.polis.service.danilaeremenko;
 
 import one.nio.http.HttpServer;
 import one.nio.http.HttpSession;
-import one.nio.http.Param;
-import one.nio.http.Path;
 import one.nio.http.Request;
 import one.nio.http.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.mail.polis.lsm.DAO;
 import ru.mail.polis.service.Service;
 
@@ -16,70 +16,70 @@ import java.util.concurrent.Executors;
 
 public class BasicService extends HttpServer implements Service {
     private final DaoWrapper daoWrapper;
-    private final Executor longTasksService = Executors.newFixedThreadPool(4);//TODO so, we need to think about order..
+    private final Executor serviceExecutor = Executors.newFixedThreadPool(4);
+    private static final Logger SERVICE_LOGGER = LoggerFactory.getLogger(BasicService.class);
 
     public BasicService(int port, DAO dao) throws IOException {
-        super(MyConfigFactory.fromPortWorkers(port, 4));
+        super(MyConfigFactory.fromPortWorkersKeepAlive(port, 4, 1));
         this.daoWrapper = new DaoWrapper(dao);
     }
 
     @Override
-    public void handleDefault(Request request, HttpSession session) throws IOException {
-        session.sendResponse(new Response(Response.BAD_REQUEST, "Not found".getBytes(StandardCharsets.UTF_8)));
+    public void handleDefault(Request request, HttpSession localSession) {
+        serviceExecutor.execute(() -> {
+            try {
+                String path = request.getPath();
+                switch (path) {
+                    case "/v0/status":
+                        processStatus(localSession);
+                        return;
+                    case "/v0/entity":
+                        processEntity(request, localSession);
+                        return;
+                    default:
+                        localSession.sendResponse(new Response(Response.BAD_REQUEST, "Not found".getBytes(StandardCharsets.UTF_8)));
+                }
+            } catch (IOException e) {
+                SERVICE_LOGGER.error("IOException caught handleDefault", e);
+            }
+        });
     }
 
-    @Path("/v0/status")
-    public Response status() {
-        return Response.ok("Okay bro");
+    public void processStatus(HttpSession localSession) throws IOException {
+        localSession.sendResponse(Response.ok("Okay bro"));
     }
 
-    @Path("/v0/entity")
-    public void entity(
+    public void processEntity(
             final Request request,
-            @Param(value = "id", required = true) final String id,
             HttpSession localSession
     ) throws IOException {
-        if (id.isBlank()) {
-            Response response = new Response(Response.BAD_REQUEST, "Undefined method".getBytes(StandardCharsets.UTF_8));
-            localSession.sendResponse(response);
+        final String id = request.getParameter("id");
+        if (id == null || id.equals("=")) {
+            localSession.sendResponse(
+                    new Response(
+                            Response.BAD_REQUEST,
+                            "Undefined method".getBytes(StandardCharsets.UTF_8)
+                    )
+            );
+            return;
         }
         switch (request.getMethod()) {
             case Request.METHOD_GET:
-                this.longTasksService.execute(() -> {
-                            Response response = this.daoWrapper.getEntity(id);
-                            try {
-                                localSession.sendResponse(response);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                );
-                break;
+                localSession.sendResponse(this.daoWrapper.getEntity(id));
+                return;
             case Request.METHOD_PUT:
-                this.longTasksService.execute(() -> {
-                            Response response = this.daoWrapper.putEntity(id, request.getBody());
-                            try {
-                                localSession.sendResponse(response);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                );
-                break;
+                localSession.sendResponse(this.daoWrapper.putEntity(id, request.getBody()));
+                return;
             case Request.METHOD_DELETE:
-                this.longTasksService.execute(() -> {
-                            Response response = this.daoWrapper.deleteEntity(id);
-                            try {
-                                localSession.sendResponse(response);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                );
-                break;
+                localSession.sendResponse(this.daoWrapper.deleteEntity(id));
+                return;
             default:
-                Response response = new Response(Response.BAD_REQUEST, "Undefined method".getBytes(StandardCharsets.UTF_8));
-                localSession.sendResponse(response);
+                localSession.sendResponse(
+                        new Response(
+                                Response.BAD_REQUEST,
+                                "Undefined method".getBytes(StandardCharsets.UTF_8)
+                        )
+                );
         }
 
     }
