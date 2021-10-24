a05470c73cd385eb28f88b046020bc5915eb1235
RafaOstrovskiy <bulasad@gmail.com>
2021-10-24 20:30:42 +0300

    refactor DAO impl

--- src/main/java/ru/mail/polis/lsm/DAOFactory.java
+++ src/main/java/ru/mail/polis/lsm/DAOFactory.java
@@ -1,10 +1,11 @@
-package ru.mail.polis.lsm;
+ package ru.mail.polis.lsm;
 
 import ru.mail.polis.lsm.artem_drozdov.LsmDAO;
 
+import ru.mail.polis.lsm.artem_drozdov.LsmDAO;
 import java.io.IOException;
 
-public final class DAOFactory {
+ public final class DAOFactory {
 
     private DAOFactory() {
         // Only static methods
