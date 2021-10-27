6d64905a8ef9b73064574bdc0478dbaf00470643
kuzo-liza <kuzo-liza@yandex.ru>
2021-10-27 23:26:31 +0300

    fix codeclimate

--- src/main/java/ru/mail/polis/lsm/artem_drozdov/SSTableHelper.java
+++ src/main/java/ru/mail/polis/lsm/artem_drozdov/SSTableHelper.java
@@ -2,7 +2,7 @@ package ru.mail.polis.lsm.artem_drozdov;
 
 import ru.mail.polis.lsm.Record;
 
-public class SSTableHelper {
+public final class SSTableHelper {
     public static final String SSTABLE_FILE_PREFIX = "file_";
     public static final String COMPACTION_FILE_NAME = "compaction";
     public static final int MAX_BUFFER_SIZE = 4096;
