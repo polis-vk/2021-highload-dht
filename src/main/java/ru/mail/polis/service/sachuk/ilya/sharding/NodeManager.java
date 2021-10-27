743d42bee8912808b0230b1e0f474ae551dbada1
IlyaAAAA <ilya-sachuk@mail.ru>
2021-10-27 22:55:11 +0300

    fixed naming

--- src/main/java/ru/mail/polis/service/sachuk/ilya/sharding/NodeManager.java
+++ src/main/java/ru/mail/polis/service/sachuk/ilya/sharding/NodeManager.java
@@ -3,26 +3,21 @@ package ru.mail.polis.service.sachuk.ilya.sharding;
 import one.nio.http.HttpClient;
 import one.nio.net.ConnectionString;
 import one.nio.util.Hash;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.Set;
-import java.util.SortedMap;
 import java.util.TreeMap;
 
 public final class NodeManager {
-    private final Logger logger = LoggerFactory.getLogger(NodeRouter.class);
-
     @SuppressWarnings("PMD")
     private static volatile NodeManager instance;
     private final VNodeConfig vnodeConfig;
     private final NavigableMap<Integer, VNode> circle;
-    private final SortedMap<String, HttpClient> clients;
+    private final NavigableMap<String, HttpClient> clients;
 
-    private NodeManager(Set<String> topology, VNodeConfig vNodeConfig) {
-        this.vnodeConfig = vNodeConfig;
+    private NodeManager(Set<String> topology, VNodeConfig vnodeConfig) {
+        this.vnodeConfig = vnodeConfig;
         circle = new TreeMap<>();
 
         clients = new TreeMap<>();
@@ -45,18 +40,14 @@ public final class NodeManager {
     }
 
     public void addNode(Node node) {
-        logger.info("server with port in add node:" + node.port);
         for (int i = 0; i < vnodeConfig.nodeWeight; i++) {
             int hashCode = Hash.murmur3(Node.HOST + node.port + i);
 
             circle.put(hashCode, new VNode(node));
         }
-        logger.info("server with port in end add node:" + node.port);
     }
 
     public VNode getNearVNode(String key) {
-        logger.info("in getNearNode");
-
         Map.Entry<Integer, VNode> integerVNodeEntry = circle.ceilingEntry(Hash.murmur3(key));
 
         VNode vNode;
@@ -66,20 +57,10 @@ public final class NodeManager {
             vNode = integerVNodeEntry.getValue();
         }
 
-        logger.info("in end getNearNode, key is: " + key);
-        logger.info("in end getNearNode, found vNode and port: " + vNode.getPhysicalNode().port);
-
         return vNode;
     }
 
     public HttpClient getHttpClient(String endpoint) {
-        Set<String> strings = clients.keySet();
-
-        for (String string : strings) {
-            logger.info("port from sortedMap: " + string);
-            logger.info(String.valueOf(endpoint.equals(string)));
-        }
-
         return clients.get(endpoint);
     }
 
