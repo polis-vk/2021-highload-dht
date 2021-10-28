01529d25fb49f588b6a61be504b645e9c064b6c1
Danila <danilaeremenko@mail.ru>
2021-10-28 15:47:02 +0300

    HL-DHT/stage4: logging added into ConsistentHash.java for future load distribution analyzing

--- src/main/java/ru/mail/polis/service/danilaeremenko/ConsistentHash.java
+++ src/main/java/ru/mail/polis/service/danilaeremenko/ConsistentHash.java
@@ -1,11 +1,15 @@
 package ru.mail.polis.service.danilaeremenko;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class ConsistentHash {
     private final SortedMap<Integer, ClusterAdapter> partitionMap = new TreeMap<>();
+    private static final Logger C_HASH_LOGGING = LoggerFactory.getLogger(ConsistentHash.class);
 
     //TODO update implementation (is a stub, I swear, I will implement another methods!)
     private int hashFunc(String str) {
@@ -15,21 +19,26 @@ public class ConsistentHash {
     public ConsistentHash(final Set<String> topology) {
         for (String topologyDesc : topology) {
             ClusterAdapter currCluster = ClusterAdapter.fromStringDesc(topologyDesc);
-            partitionMap.put(hashFunc(topologyDesc), currCluster);
+            int topologyHash = hashFunc(topologyDesc);
+            partitionMap.put(topologyHash, currCluster);
+            C_HASH_LOGGING.debug("PARTITIONING: {}, {}", topologyHash, topologyDesc);
         }
     }
 
     public ClusterAdapter getClusterAdapter(String recordKey) {
         int keyHash = hashFunc(recordKey);
-        SortedMap<Integer, ClusterAdapter> subMap = partitionMap.tailMap(keyHash);
-        if (subMap.isEmpty()) {
+        SortedMap<Integer, ClusterAdapter> rightNodesMap = partitionMap.tailMap(keyHash);
+        final ClusterAdapter targetAdapter;
+        if (rightNodesMap.isEmpty()) {
             //hash lies between last and first nodes
             Integer i = partitionMap.firstKey();
-            return partitionMap.get(i);
+            targetAdapter = partitionMap.get(i);
         } else {
             //ordinary case
-            Integer i = subMap.firstKey();
-            return subMap.get(i);
+            Integer i = rightNodesMap.firstKey();
+            targetAdapter = rightNodesMap.get(i);
         }
+        C_HASH_LOGGING.debug("KEY_HASH = {}, TARGET_NODE = {}", keyHash, targetAdapter.toURL());
+        return targetAdapter;
     }
 }
