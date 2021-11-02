0b6583b9d28b058a48f6205a4c9c878a1912c71d
Shabinsky Dmitry <sdimosikvip@gmail.com>
2021-11-02 18:12:20 +0300

    add sharding from the lecture

--- src/main/java/ru/mail/polis/service/shabinsky_dmitry/BasicService.java
+++ src/main/java/ru/mail/polis/service/shabinsky_dmitry/BasicService.java
@@ -4,6 +4,7 @@ import one.nio.http.*;
 import one.nio.net.ConnectionString;
 import one.nio.pool.PoolException;
 import one.nio.server.AcceptorConfig;
+import one.nio.util.Hash;
 import one.nio.util.Utf8;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,24 +13,20 @@ import ru.mail.polis.lsm.Record;
 import ru.mail.polis.service.Service;
 
 import java.io.IOException;
-import java.math.BigInteger;
+import java.net.InetAddress;
 import java.nio.ByteBuffer;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.concurrent.Executor;
 
 public final class BasicService extends HttpServer implements Service {
 
     private static final Logger LOG = LoggerFactory.getLogger(BasicService.class);
+    private static final int TIMEOUT = 100;
     private final DAO dao;
     private final Executor executor;
 
-    private final BigInteger currentNodeHash;
-    private final SortedMap<BigInteger, HttpClient> ring;
+    private final List<String> topology;
+    private final Set<String> me = new HashSet<>();
 
     public BasicService(
         final int port,
@@ -42,11 +39,27 @@ public final class BasicService extends HttpServer implements Service {
         this.dao = dao;
         this.executor = executor;
 
-        this.currentNodeHash = hashMD5(ByteBuffer.wrap(toBytes("http://localhost:" + port)));
-        this.ring = new TreeMap<>();
+        this.topology = new ArrayList<>(topology);
+        Collections.sort(this.topology);
 
-        topology.forEach(node ->
-            ring.put(hashMD5(ByteBuffer.wrap(toBytes(node))), new HttpClient(new ConnectionString(node))));
+
+        List<InetAddress> allMe = Arrays.asList(InetAddress.getAllByName(null));
+
+        for (String node : topology) {
+            ConnectionString connection = new ConnectionString(node);
+            if (connection.getPort() != port) {
+                continue;
+            }
+
+            List<InetAddress> nodeAddresses = Arrays.asList(
+                InetAddress.getAllByName(connection.getHost())
+            ); //TODO optimize
+            nodeAddresses.retainAll(allMe);
+
+            if (!nodeAddresses.isEmpty()) {
+                me.add(node);
+            }
+        }
     }
 
     private static HttpServerConfig from(final int port) {
@@ -69,13 +82,7 @@ public final class BasicService extends HttpServer implements Service {
         Request request,
         @Param(value = "id", required = true) final String id
     ) {
-        execute(request, session, () -> {
-
-            final ByteBuffer key = ByteBuffer.wrap(toBytes(id));
-            HttpClient client = getClient(key);
-            if (client != null) {
-                return redirectRequest(client, request);
-            }
+        execute(id, request, session, () -> {
 
             if (id.isBlank()) {
                 return new Response(Response.BAD_REQUEST, toBytes("Bad id"));
@@ -131,15 +138,46 @@ public final class BasicService extends HttpServer implements Service {
         return result;
     }
 
+    private HttpClient forKey(String id) {
+
+        String bestMatch = null;
+        int maxHash = Integer.MIN_VALUE;
+
+        for (String option : topology) {
+            int hash = Hash.murmur3(option + id);
+
+            if (hash > maxHash) { // it is important to have topology consistently sorted
+                bestMatch = option;
+                maxHash = hash;
+            }
+        }
+
+        if (bestMatch == null) {
+            throw new IllegalStateException("No nodes?");
+        }
+
+        if (me.contains(bestMatch)) {
+            return null;
+        }
+
+        return new HttpClient(new ConnectionString(bestMatch));
+    }
+
     private byte[] toBytes(String text) {
         return Utf8.toBytes(text);
     }
 
-    private void execute(Request request, HttpSession session, Task task) {
+    private void execute(String id, Request request, HttpSession session, Task task) {
         executor.execute(() -> {
+
             Response call;
             try {
-                call = task.call();
+                HttpClient client = forKey(id);
+                if (client != null) {
+                    call = client.invoke(request, TIMEOUT);
+                } else {
+                    call = task.call();
+                }
             } catch (Exception e) {
                 LOG.error("Unexpected exception during method call {}", request.getMethodName(), e);
                 sendResponse(session, new Response(Response.INTERNAL_ERROR, toBytes("Something wrong")));
@@ -158,19 +196,6 @@ public final class BasicService extends HttpServer implements Service {
         }
     }
 
-    private HttpClient getClient(ByteBuffer key) {
-        BigInteger hash = hashMD5(key);
-        SortedMap<BigInteger, HttpClient> subMap = ring.tailMap(hash);
-
-        if (subMap.isEmpty()) {
-            BigInteger i = ring.firstKey();
-            return i.equals(currentNodeHash) ? null : ring.get(i);
-        }
-
-        BigInteger i = subMap.firstKey();
-        return i.equals(currentNodeHash) ? null : subMap.get(i);
-    }
-
     private void sendResponse(HttpSession session, Response call) {
         try {
             session.sendResponse(call);
@@ -182,17 +207,5 @@ public final class BasicService extends HttpServer implements Service {
     @FunctionalInterface
     private interface Task {
         Response call();
-    }
-
-    private BigInteger hashMD5(ByteBuffer key) {
-        try {
-            MessageDigest m = MessageDigest.getInstance("MD5");
-            m.reset();
-            m.update(key);
-            byte[] digest = m.digest();
-            return new BigInteger(1, digest);
-        } catch (NoSuchAlgorithmException ignored) {
-            return null;
-        }
     }
 }
