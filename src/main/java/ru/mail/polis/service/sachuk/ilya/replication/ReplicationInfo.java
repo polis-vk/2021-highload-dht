6daa6c3cf89794f01ced0357f8738d87b1a103d3
IlyaAAAA <ilya-sachuk@mail.ru>
2021-11-06 22:25:13 +0300

    added

---
+++ src/main/java/ru/mail/polis/service/sachuk/ilya/replication/ReplicationInfo.java
@@ -0,0 +1,28 @@
+package ru.mail.polis.service.sachuk.ilya.replication;
+
+import java.util.Arrays;
+
+public class ReplicationInfo {
+    public final int ask;
+    public final int from;
+
+    private ReplicationInfo(int ask, int from) {
+        this.ask = ask;
+        this.from = from;
+    }
+
+    public static ReplicationInfo from(String replicas) {
+        //replicas={2/2}
+//        String[] split = replicas.split("/");
+        int[] arr = Arrays.stream(replicas.split("/")).mapToInt(Integer::parseInt).toArray();
+
+
+        return new ReplicationInfo(arr[0], arr[1]);
+    }
+
+    public static ReplicationInfo from(int nodeNumber) {
+        int ask = (nodeNumber / 2) + 1;
+
+        return new ReplicationInfo(ask, nodeNumber);
+    }
+}
