61580d842d7d4147c2d698273b2bcb825921bad7
Luc Prucell <31856838+LucasMbele@users.noreply.github.com>
2021-12-13 18:35:42 +0300

    Update ThreadPoolExecutorUtil.java

--- src/main/java/ru/mail/polis/service/lucas_mbele/ThreadPoolExecutorUtil.java
+++ src/main/java/ru/mail/polis/service/lucas_mbele/ThreadPoolExecutorUtil.java
@@ -2,20 +2,19 @@ package ru.mail.polis.service.lucas_mbele;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolExecutorUtil {
-
     private Logger logger = LoggerFactory.getLogger(ThreadPoolExecutorUtil.class);
     private final int corePoolSize;
     private final int maximumPoolSize = 4;
     private final long keepAliveTime;
     private final int capacity;
     private ThreadPoolExecutor threadPoolExecutor;
-
-
-    public static ThreadPoolExecutorUtil init(){
+    
+    public static ThreadPoolExecutorUtil init() {
         return new ThreadPoolExecutorUtil(2,Long.MAX_VALUE,100);
     }
 
@@ -24,12 +23,14 @@ public class ThreadPoolExecutorUtil {
         this.corePoolSize = corePoolSize;
         this.keepAliveTime = keepAliveTime;
         BlockingQueue<Runnable> blockingQueue =  new ArrayBlockingQueue<>(this.capacity);
-        threadPoolExecutor = new ThreadPoolExecutor(this.corePoolSize, this.maximumPoolSize, this.keepAliveTime, TimeUnit.NANOSECONDS, blockingQueue);
+        threadPoolExecutor = new ThreadPoolExecutor(this.corePoolSize, this.maximumPoolSize, 
+                                                    this.keepAliveTime, TimeUnit.NANOSECONDS, 
+                                                    blockingQueue);
         threadPoolExecutor.prestartAllCoreThreads();
         handleRejections();
     }
 
-    public void handleRejections(){
+    public void handleRejections() {
         threadPoolExecutor.setRejectedExecutionHandler((task, executor) -> {
             try {
                 Thread.sleep(1000);
@@ -42,7 +43,7 @@ public class ThreadPoolExecutorUtil {
         });
     }
 
-    public void executeTask(Runnable task){
+    public void executeTask(Runnable task) {
             threadPoolExecutor.execute(task);
     }
 }
