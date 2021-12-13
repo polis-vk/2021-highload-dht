7564c114216100fa98ccd89384305bc2edb171e5
Luc Prucell <31856838+LucasMbele@users.noreply.github.com>
2021-12-13 18:35:36 +0300

    Update RendezvousHashingImpl.java

--- src/main/java/ru/mail/polis/service/lucas_mbele/RendezvousHashingImpl.java
+++ src/main/java/ru/mail/polis/service/lucas_mbele/RendezvousHashingImpl.java
@@ -3,11 +3,12 @@ package ru.mail.polis.service.lucas_mbele;
 import java.nio.charset.StandardCharsets;
 import java.util.Set;
 
-public class RendezvousHashingImpl implements RendezvousHashing{
+public class RendezvousHashingImpl implements RendezvousHashing {
 
     private final Set<String> topology;
     private final Murmur2HashImpl hashFunction;
-    public RendezvousHashingImpl(Set<String>topology){
+    
+    public RendezvousHashingImpl(Set<String> topology) {
         this.hashFunction = new Murmur2HashImpl();
         this.topology = topology;
     }
@@ -16,14 +17,14 @@ public class RendezvousHashingImpl implements RendezvousHashing{
     public String getResponsibleNode(String key) {
         int maxValue = Integer.MIN_VALUE;
         String responsibleNode = "";
-        if (this.topology.isEmpty()){
+        if (this.topology.isEmpty()) {
             return null;
         }
         int score;
-        for(String node : topology){
-            String nodeKey = key+node;
+        for (String node : topology) {
+            String nodeKey = key + node;
             score =  hashFunction.hash32(nodeKey.getBytes(StandardCharsets.UTF_8),key.length());
-            if(score > maxValue){
+            if (score > maxValue) {
                 responsibleNode = node;
                 maxValue = score;
             }
