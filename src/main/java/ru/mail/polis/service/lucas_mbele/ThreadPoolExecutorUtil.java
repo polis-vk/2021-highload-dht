b6b961caf679262b7cd7fc004ffd56da8477ef2e
Luc Prucell <31856838+LucasMbele@users.noreply.github.com>
2021-12-13 19:03:32 +0300

    Update ThreadPoolExecutorUtil.java

--- src/main/java/ru/mail/polis/service/lucas_mbele/ThreadPoolExecutorUtil.java
+++ src/main/java/ru/mail/polis/service/lucas_mbele/ThreadPoolExecutorUtil.java
@@ -2,7 +2,7 @@ package ru.mail.polis.service.lucas_mbele;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.concurrent.BlockingQueue
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
