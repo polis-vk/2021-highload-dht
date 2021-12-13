52930b2a2f0e45c0d4fcfe7411255e849288f06e
Luc Prucell <31856838+LucasMbele@users.noreply.github.com>
2021-12-13 19:36:30 +0300

    Update ThreadPoolExecutorUtil.java

--- src/main/java/ru/mail/polis/service/lucas_mbele/ThreadPoolExecutorUtil.java
+++ src/main/java/ru/mail/polis/service/lucas_mbele/ThreadPoolExecutorUtil.java
@@ -10,7 +10,6 @@ import java.util.concurrent.TimeUnit;
 public final class ThreadPoolExecutorUtil {
     public final Logger logger = LoggerFactory.getLogger(ThreadPoolExecutorUtil.class);
     public final int corePoolSize;
-    public static final int pool = 4;
     public final long keepAliveTime;
     public final int capacity;
     public final ThreadPoolExecutor threadPoolExecutor;
@@ -24,7 +23,7 @@ public final class ThreadPoolExecutorUtil {
         this.corePoolSize = corePoolSize;
         this.keepAliveTime = keepAliveTime;
         BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<>(this.capacity);
-        threadPoolExecutor = new ThreadPoolExecutor(this.corePoolSize, this.pool, 
+        threadPoolExecutor = new ThreadPoolExecutor(this.corePoolSize, 4, 
                                                     this.keepAliveTime, TimeUnit.NANOSECONDS, 
                                                     blockingQueue);
         threadPoolExecutor.prestartAllCoreThreads();
