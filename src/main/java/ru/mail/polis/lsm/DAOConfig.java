99d3abc390eafaa6c80b3296a506c9e9bbe00837
Luc Prucell <31856838+LucasMbele@users.noreply.github.com>
2021-12-13 17:37:47 +0300

    Update DAOConfig.java

--- src/main/java/ru/mail/polis/lsm/DAOConfig.java
+++ src/main/java/ru/mail/polis/lsm/DAOConfig.java
@@ -20,7 +20,7 @@ import java.nio.file.Path;
 
 public class DAOConfig {
     public static final int DEFAULT_MEMORY_LIMIT = 4 * 1024 * 1024;
-    public static final int DEFAULT_MAX_TABLES = 4 ;
+    public static final int DEFAULT_MAX_TABLES = 4;
     public final Path dir;
     public final int memoryLimit;
     public final int maxTables;
@@ -32,7 +32,7 @@ public class DAOConfig {
         this(dir, memoryLimit, DEFAULT_MAX_TABLES);
     }
 
-    public DAOConfig(Path dir, int memoryLimit,int maxTables) {
+    public DAOConfig(Path dir, int memoryLimit, int maxTables) {
         this.dir = dir;
         this.memoryLimit = memoryLimit;
         this.maxTables = maxTables;
