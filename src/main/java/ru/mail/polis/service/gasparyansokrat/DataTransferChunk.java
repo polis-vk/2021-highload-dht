8e3caf84f27652a8a4402c54c46792c1350c7566
Gasparyan0xff <kantkerry@mail.ru>
2021-12-18 17:33:54 +0300

    fix notes

--- src/main/java/ru/mail/polis/service/gasparyansokrat/DataTransferChunk.java
+++ src/main/java/ru/mail/polis/service/gasparyansokrat/DataTransferChunk.java
@@ -3,21 +3,19 @@ package ru.mail.polis.service.gasparyansokrat;
 import ru.mail.polis.lsm.Record;
 
 import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 
 final class DataTransferChunk {
 
     private final Iterator<Record> data;
-    private Iterator<Record> current;
     private boolean isEnd;
 
-    public static final String ENDS = "0\r\n\r\n";
+    public static final byte[] ENDS = "0\r\n\r\n".getBytes(StandardCharsets.UTF_8);
+    public static final byte[] NEXT_LINE = "\r\n".getBytes(StandardCharsets.UTF_8);
 
     private DataTransferChunk(final Iterator<Record> data) {
         this.data = data;
-        this.current = data;
         this.isEnd = false;
     }
 
@@ -29,48 +27,43 @@ final class DataTransferChunk {
         return data;
     }
 
-    public void refresh() {
-        current = data;
-        isEnd = false;
-    }
-
     public byte[] getChunk() {
         if (isEnd) {
             return new byte[0];
         }
-        CharBuffer chunk;
-        if (current.hasNext()) {
-            chunk = buildChunk(current.next());
+        ByteBuffer chunk;
+        if (data.hasNext()) {
+            chunk = buildChunk(data.next());
         } else {
             chunk = endChunk();
             isEnd = true;
         }
-        return char2byteBuffer(chunk.asReadOnlyBuffer()).array();
+        return chunk.array();
     }
 
-    private ByteBuffer char2byteBuffer(final CharBuffer buffer) {
-        ByteBuffer tmpBuffer = ByteBuffer.allocate(buffer.limit());
-        tmpBuffer.put(buffer.toString().getBytes(StandardCharsets.UTF_8));
-        return tmpBuffer.position(0);
-    }
-
-    private CharBuffer buildChunk(final Record record) {
-        final String key = StandardCharsets.UTF_8.decode(record.getKey()).toString();
-        final String value = StandardCharsets.UTF_8.decode(record.getValue()).toString();
-        final String chunkSize = Integer.toHexString(key.length() + value.length() + 1);
-        final int totalSize = chunkSize.length() + key.length() + value.length() + 5 * Byte.BYTES;
-        CharBuffer tmpChunk = CharBuffer.allocate(totalSize);
-        tmpChunk.put(chunkSize);
-        tmpChunk.put("\r\n");
+    private ByteBuffer buildChunk(final Record record) {
+        final byte[] key = extractBytes(record.getKey());
+        final byte[] value = extractBytes(record.getValue());
+        final byte[] hexSize = Integer.toHexString(key.length + value.length + 1).getBytes(StandardCharsets.UTF_8);
+        final int totalSize = hexSize.length + key.length + value.length + 5 * Byte.BYTES;
+        ByteBuffer tmpChunk = ByteBuffer.allocate(totalSize);
+        tmpChunk.put(hexSize);
+        tmpChunk.put(NEXT_LINE);
         tmpChunk.put(key);
-        tmpChunk.put('\n');
+        tmpChunk.put("\n".getBytes(StandardCharsets.UTF_8));
         tmpChunk.put(value);
-        tmpChunk.put("\r\n");
+        tmpChunk.put(NEXT_LINE);
         return tmpChunk.position(0);
     }
 
-    private CharBuffer endChunk() {
-        CharBuffer tmpChunk = CharBuffer.allocate(ENDS.length());
+    private byte[] extractBytes(final ByteBuffer buffer) {
+        byte[] tmp = new byte[buffer.limit()];
+        buffer.get(tmp);
+        return tmp;
+    }
+
+    private ByteBuffer endChunk() {
+        ByteBuffer tmpChunk = ByteBuffer.allocate(ENDS.length);
         tmpChunk.put(ENDS);
         return tmpChunk.position(0);
     }
